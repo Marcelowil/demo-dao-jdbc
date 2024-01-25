0230a0b4a85f0a1e78fb81b34fecd64505a6478b
Marcelo William <marcelo.willian@live.com>
2024-01-25 07:57:41 -0300

    Create Program2 and DepartmentDaoJDBC

---
+++ src/application/Program2.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("=== TEST 1: Deparment findById ====");
+		
+		sc.close();
+	}
+
+}
